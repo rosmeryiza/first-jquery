7886c2c5899050d7d75cce1af88a445fa6774924
rosmeryiza <rosmeryizaguirre@gmail.com>
2018-11-09 23:46:12 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+
+  $('#toggle_honey').on('click', ()=> {
+    $('#honeybee').toggle();
+  });
+
+  $('#toggle_bumble').on('click', ()=> {
+    $('#bumblebee').toggle();
+  });
+
+  $('#toggle_mining').on('click', ()=> {
+    $('#mining').toggle();
+  });
+
+  $('#toggle_carpenter').on('click', ()=> {
+    $('#carpenter').toggle();
+  });
+
+});
